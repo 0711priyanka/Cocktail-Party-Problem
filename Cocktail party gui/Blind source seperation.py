072b01dc53d086c1f47e6c6bece6ab536797487e
0711priyanka <48524984+0711priyanka@users.noreply.github.com>
2021-01-24 15:48:38 +0530

    Cocktail Party Problem using FastICA

---
+++ Cocktail party gui/Blind source seperation.py
@@ -0,0 +1,600 @@
+# -*- coding: utf-8 -*-
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QMovie
+import os, subprocess
+import shutil
+from backend.fastICA_no_sklearn import convert
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("Blind Source Seperator")
+        MainWindow.setFixedSize(643, 385)
+        MainWindow.setStyleSheet("QWidget\n"
+"{\n"
+"    background-color: qlineargradient(spread:repeat, x1:1, y1:0, x2:1, y2:1, stop:0 rgba(27, 39, 50, 255),stop:1 rgba(47, 53, 74, 255));\n"
+"    color: #000000;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"/*-----QLabel-----*/\n"
+"QLabel\n"
+"{\n"
+"    background-color: transparent;\n"
+"    color: #c2c7d5;\n"
+"    font-size: 30px;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"/*-----QPushButton-----*/\n"
+"QPushButton\n"
+"{\n"
+"    background-color: qlineargradient(spread:pad, x1:0, y1:0.511, x2:1, y2:0.511, stop:0 rgba(89, 130, 192, 255),stop:0.995192 rgba(43, 149, 230, 255));\n"
+"    color: #fff;\n"
+"    font-size: 14px;\n"
+"    font-weight: bold;\n"
+"    border: none;\n"
+"    border-radius: 8px;\n"
+"    padding: 5px;\n"
+"\n"
+"}\n"
+"\n"
+"QPushButton:disabled {\n"
+"	 background-color:#aaa;\n"
+"}\n"
+"\n"
+"QPushButton::pressed\n"
+"{\n"
+"    background-color: qlineargradient(spread:pad, x1:0, y1:0.511, x2:1, y2:0.511, stop:0 rgba(0, 207, 179, 255),stop:1 rgba(70, 255, 230, 255));\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"/*-----QCheckBox-----*/\n"
+"QCheckBox\n"
+"{\n"
+"    background-color: transparent;\n"
+"    color: #fff;\n"
+"    font-size: 10px;\n"
+"    font-weight: bold;\n"
+"    border: none;\n"
+"    border-radius: 5px;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"/*-----QCheckBox-----*/\n"
+"QCheckBox::indicator\n"
+"{\n"
+"    color: #b1b1b1;\n"
+"    background-color: #323232;\n"
+"    border: 1px solid darkgray;\n"
+"    width: 12px;\n"
+"    height: 12px;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QCheckBox::indicator:checked\n"
+"{\n"
+"    image:url(\"./ressources/check.png\");\n"
+"    background-color: qlineargradient(spread:pad, x1:0, y1:0.511, x2:1, y2:0.511, stop:0 rgba(0, 172, 149, 255),stop:0.995192 rgba(54, 197, 177, 255));;\n"
+"    border: 1px solid #607cff;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QCheckBox::indicator:unchecked:hover\n"
+"{\n"
+"    border: 1px solid #08b099;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QCheckBox::disabled\n"
+"{\n"
+"    color: #656565;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QCheckBox::indicator:disabled\n"
+"{\n"
+"    background-color: #656565;\n"
+"    color: #656565;\n"
+"    border: 1px solid #656565;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"/*-----QLineEdit-----*/\n"
+"QLineEdit\n"
+"{\n"
+"    background-color: #c2c7d5;\n"
+"    color: #000;\n"
+"    font-weight: bold;\n"
+"    border: none;\n"
+"    border-radius: 2px;\n"
+"    padding: 3px;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"/*-----QListView-----*/\n"
+"QListView\n"
+"{\n"
+"    background-color: qlineargradient(spread:pad, x1:1, y1:0, x2:1, y2:1, stop:0 rgba(50, 61, 80, 255),stop:1 rgba(44, 49, 69, 255));\n"
+"    color: #fff;\n"
+"    font-size: 12px;\n"
+"    font-weight: bold;\n"
+"    border: 1px solid #191919;\n"
+"    show-decoration-selected: 0;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QListView::item\n"
+"{\n"
+"    color: #31cecb;\n"
+"    background-color: #454e5e;\n"
+"    border: none;\n"
+"    padding: 5px;\n"
+"    border-radius: 0px;\n"
+"    padding-left : 10px;\n"
+"    height: 42px;\n"
+"\n"
+"}\n"
+"\n"
+"QListView::item:selected\n"
+"{\n"
+"    color: #31cecb;\n"
+"    background-color: #454e5e;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QListView::item:!selected\n"
+"{\n"
+"    color:white;\n"
+"    background-color: transparent;\n"
+"    border: none;\n"
+"    padding-left : 10px;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QListView::item:!selected:hover\n"
+"{\n"
+"    color: #bbbcba;\n"
+"    background-color: #454e5e;\n"
+"    border: none;\n"
+"    padding-left : 10px;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"/*-----QTreeView-----*/\n"
+"QTreeView \n"
+"{\n"
+"    background-color: #232939;\n"
+"    show-decoration-selected: 0;\n"
+"    color: #c2c8d7;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QTreeView::item \n"
+"{\n"
+"    border-top-color: transparent;\n"
+"    border-bottom-color: transparent;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QTreeView::item:hover \n"
+"{\n"
+"    background-color: #606060;\n"
+"    color: #fff;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QTreeView::item:selected \n"
+"{\n"
+"    background-color: #0ab19a;\n"
+"    color: #fff;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QTreeView::item:selected:active\n"
+"{\n"
+"    background-color: #0ab19a;\n"
+"    color: #fff;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QTreeView::branch:has-children:!has-siblings:closed,\n"
+"QTreeView::branch:closed:has-children:has-siblings \n"
+"{\n"
+"    image: url(://tree-closed.png);\n"
+"\n"
+"}\n"
+"\n"
+"QTreeView::branch:open:has-children:!has-siblings,\n"
+"QTreeView::branch:open:has-children:has-siblings  \n"
+"{\n"
+"    image: url(://tree-open.png);\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"/*-----QTableView & QTableWidget-----*/\n"
+"QTableView\n"
+"{\n"
+"    background-color: #232939;\n"
+"    border: 1px solid gray;\n"
+"    color: #f0f0f0;\n"
+"    gridline-color: #232939;\n"
+"    outline : 0;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QTableView::disabled\n"
+"{\n"
+"    background-color: #242526;\n"
+"    border: 1px solid #32414B;\n"
+"    color: #656565;\n"
+"    gridline-color: #656565;\n"
+"    outline : 0;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QTableView::item:hover \n"
+"{\n"
+"    background-color: #606060;\n"
+"    color: #f0f0f0;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QTableView::item:selected \n"
+"{\n"
+"    background-color: #0ab19a;\n"
+"    color: #F0F0F0;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QTableView::item:selected:disabled\n"
+"{\n"
+"    background-color: #1a1b1c;\n"
+"    border: 2px solid #525251;\n"
+"    color: #656565;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QTableCornerButton::section\n"
+"{\n"
+"    background-color: #343a49;\n"
+"    color: #fff;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QHeaderView::section\n"
+"{\n"
+"    color: #fff;\n"
+"    border-top: 0px;\n"
+"    border-bottom: 1px solid gray;\n"
+"    border-right: 1px solid gray;\n"
+"    background-color: #343a49;\n"
+"    margin-top:1px;\n"
+"    margin-bottom:1px;\n"
+"    padding: 5px;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QHeaderView::section:disabled\n"
+"{\n"
+"    background-color: #525251;\n"
+"    color: #656565;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QHeaderView::section:checked\n"
+"{\n"
+"    color: #fff;\n"
+"    background-color: #0ab19a;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QHeaderView::section:checked:disabled\n"
+"{\n"
+"    color: #656565;\n"
+"    background-color: #525251;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QHeaderView::section::vertical::first,\n"
+"QHeaderView::section::vertical::only-one\n"
+"{\n"
+"    border-top: 1px solid #353635;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QHeaderView::section::vertical\n"
+"{\n"
+"    border-top: 1px solid #353635;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QHeaderView::section::horizontal::first,\n"
+"QHeaderView::section::horizontal::only-one\n"
+"{\n"
+"    border-left: 1px solid #353635;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QHeaderView::section::horizontal\n"
+"{\n"
+"    border-left: 1px solid #353635;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"/*-----QScrollBar-----*/\n"
+"QScrollBar:horizontal \n"
+"{\n"
+"    background-color: transparent;\n"
+"    height: 8px;\n"
+"    margin: 0px;\n"
+"    padding: 0px;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QScrollBar::handle:horizontal \n"
+"{\n"
+"    border: none;\n"
+"    min-width: 100px;\n"
+"    background-color: #56576c;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QScrollBar::add-line:horizontal, \n"
+"QScrollBar::sub-line:horizontal,\n"
+"QScrollBar::add-page:horizontal, \n"
+"QScrollBar::sub-page:horizontal \n"
+"{\n"
+"    width: 0px;\n"
+"    background-color: transparent;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QScrollBar:vertical \n"
+"{\n"
+"    background-color: transparent;\n"
+"    width: 8px;\n"
+"    margin: 0;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QScrollBar::handle:vertical \n"
+"{\n"
+"    border: none;\n"
+"    min-height: 100px;\n"
+"    background-color: #56576c;\n"
+"\n"
+"}\n"
+"\n"
+"\n"
+"QScrollBar::add-line:vertical, \n"
+"QScrollBar::sub-line:vertical,\n"
+"QScrollBar::add-page:vertical, \n"
+"QScrollBar::sub-page:vertical \n"
+"{\n"
+"    height: 0px;\n"
+"    background-color: transparent;\n"
+"\n"
+"}\n"
+"")
+        MainWindow.setIconSize(QtCore.QSize(40, 40))
+        MainWindow.setAnimated(True)
+        MainWindow.setDocumentMode(True)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(184, 90, 331, 61))
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic Medium")
+        font.setPointSize(-1)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setFocusPolicy(QtCore.Qt.NoFocus)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("—Pngtree—black straight arrow clip art_5575752.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton.setIcon(icon)
+        self.pushButton.setIconSize(QtCore.QSize(38, 38))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(184, 170, 331, 61))
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic Medium")
+        font.setPointSize(-1)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.pushButton_2.setIcon(icon)
+        self.pushButton_2.setIconSize(QtCore.QSize(38, 38))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(480, 270, 121, 41))
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic Medium")
+        font.setPointSize(-1)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_3.setFont(font)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QtCore.QRect(184, 270, 121, 41))
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic Medium")
+        font.setPointSize(-1)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_4.setFont(font)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(330, 270, 121, 41))
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic Medium")
+        font.setPointSize(-1)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_5.setFont(font)
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(180, 20, 201, 41))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(10, 20, 160, 341))
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        self.movie = QMovie("Cosmic.gif")
+        self.label_2.setMovie(self.movie)
+        self.movie.start()
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(10, 262, 158, 91))
+        font = QtGui.QFont()
+        font.setFamily("Yu Gothic")
+        font.setPointSize(-1)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "  Mix 1\n"
+"  Open the 1st file to be converted"))
+        self.pushButton_2.setText(_translate("MainWindow", "  Mix 2\n"
+"  Open the 2nd file to be converted"))
+        self.pushButton_3.setText(_translate("MainWindow", "Reset"))
+        self.pushButton_4.setText(_translate("MainWindow", "Convert"))
+        self.pushButton_5.setText(_translate("MainWindow", "Display "))
+        self.label.setText(_translate("MainWindow", "Upload Files"))
+        self.label_3.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:16pt; font-weight:600;\">Blind  Source</span></p><p><span style=\" font-size:16pt; font-weight:600;\">Seperator</span></p></body></html>"))
+        self.pushButton.clicked.connect(self.button_1_clicked)
+        self.pushButton_2.clicked.connect(self.button_2_clicked)
+        self.pushButton_4.clicked.connect(self.button_4_clicked)
+        self.pushButton_3.clicked.connect(self.button_3_clicked)
+        self.pushButton_5.clicked.connect(self.button_5_clicked)
+
+        self.pushButton_2.setEnabled(False)
+        self.pushButton_3.setEnabled(False)
+        self.pushButton_4.setEnabled(False)
+        self.pushButton_5.setEnabled(False)
+
+
+    def button_1_clicked(self):
+    
+        self.pushButton_2.setEnabled(True)
+        self.move_files()
+
+
+    def button_2_clicked(self):
+
+        self.pushButton_4.setEnabled(True)
+        self.move_files()
+
+    def button_4_clicked(self):
+        self.pushButton_3.setEnabled(False)
+        self.pushButton.setEnabled(False)
+        self.pushButton_2.setEnabled(False)
+        self.pushButton_5.setEnabled(True) 
+        self.pushButton_4.setEnabled(False)
+        convert()
+
+
+    def button_3_clicked(self):
+        self.pushButton_4.setEnabled(False)
+        self.pushButton.setEnabled(True)
+        self.pushButton_2.setEnabled(False)
+        self.pushButton_5.setEnabled(False)
+        self.pushButton_3.setEnabled(False)
+
+        self.reset()
+
+    def button_5_clicked(self):
+    	self.display()
+    	subprocess.run(['explorer', os.path.realpath('H:/Cocktail party gui/backend/converted_audio/')])
+    	self.pushButton_3.setEnabled(True)
+
+
+
+    def move_files(self):
+        filename = QFileDialog.getOpenFileName()
+        path = filename[0]
+        print(path)
+        if path == "" : 
+            return
+        dst = "H:/Cocktail party gui/backend/converted_audio/"
+        src = path
+
+
+        shutil.copy(src=src, dst=dst)
+
+
+    def reset(self):
+    	dst = "H:/Cocktail party gui/backend/converted_audio/"
+
+    	files = os.listdir(dst)
+    	for singlefile in files:
+       		os.remove(dst+singlefile)
+
+    def display(self):
+    	dst = "H:/Cocktail party gui/backend/converted_audio/"
+
+    	files = os.listdir(dst)
+    	for singlefile in files:
+    		if not singlefile.startswith("separate"):
+    			os.remove(dst+singlefile)
+
+       
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+ 
